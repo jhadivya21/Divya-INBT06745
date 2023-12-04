55e743c6cc85ffeb02041526850eaeefc9182c01
Divya Jha <divyajha038@gmail.com>
2023-12-05 00:17:06 +0530

    added about

--- Major Task/script.js
+++ Major Task/script.js
@@ -22,3 +22,55 @@ window.onload = function() {
     element.onmouseout = function() {
       element.style.backgroundColor = 'blue';
     }}
+
+    const stars = document.querySelectorAll('.star');
+const ratingValue = document.getElementById('ratingValue');
+
+stars.forEach(star => {
+  star.addEventListener('click', () => {
+    const value = star.getAttribute('data-value');
+    ratingValue.textContent = `You rated ${value} stars!`;
+    removeActiveStars();
+    setActiveStars(value);
+  });
+  
+  star.addEventListener('mouseover', () => {
+    const value = star.getAttribute('data-value');
+    highlightStars(value);
+  });
+
+  star.addEventListener('mouseleave', () => {
+    removeActiveStars();
+  });
+});
+
+function setActiveStars(value) {
+  for (let i = 0; i < value; i++) {
+    stars[i].classList.add('active');
+  }
+}
+
+function highlightStars(value) {
+  for (let i = 0; i < value; i++) {
+    stars[i].classList.add('active');
+  }
+}
+
+function removeActiveStars() {
+  stars.forEach(star => {
+    star.classList.remove('active');
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const stars = document.querySelectorAll('.star-rating .star');
+  
+    stars.forEach(function(star) {
+      star.addEventListener('click', function() {
+        const rating = this.getAttribute('data-value');
+        this.parentNode.setAttribute('data-rating', rating);
+        // You can perform further actions here, like sending the rating to a server.
+      });
+    });
+  });
+  
